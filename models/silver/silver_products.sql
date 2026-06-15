1e41686bb9658e1ad86678741527954e90a51d4d
Megha Sony <meghasony1997@gmail.com>
2026-06-15 15:25:07 +0530

    Update silver_products.sql

--- models/silver/silver_products.sql
+++ models/silver/silver_products.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='table', tags=['silver']) }}
 
-SELECT *
-FROM {{ dbt_utils.safe_cast(
-    relation='bronze_products',
-    field='price',
-    data_type='float'
-) }}
-WHERE price IS NOT NULL
+SELECT
+    product_id,
+    product_name,
+    category,
+    {{ dbt_utils.safe_cast("price", api.Column.translate_type("float")) }} AS price
+FROM bronze_products
+WHERE {{ dbt_utils.safe_cast("price", api.Column.translate_type("float")) }} IS NOT NULL
