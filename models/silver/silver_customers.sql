892ee2f652b9fbf14c04645cd4829432f2b453c8
Megha Sony <meghasony1997@gmail.com>
2026-06-15 16:44:50 +0530

    Update silver_customers.sql

--- models/silver/silver_customers.sql
+++ models/silver/silver_customers.sql
@@ -1,16 +1,11 @@
 {{ config(materialized='table', tags=['silver']) }}
 
-WITH cleaned AS (
-
-    SELECT
-        customer_id,
-        UPPER(LEFT(customer_name, 1)) + LOWER(SUBSTRING(customer_name, 2, LEN(customer_name))) AS customer_name,
-        {{ dbt_utils.coalesce([
-            "NULLIF(LTRIM(RTRIM(country)), '')",
-            "'UNKNOWN'"
-        ]) }} AS country
-    FROM bronze_customers
-
-)
-
-SELECT DISTINCT * FROM cleaned
+SELECT
+    customer_id,
+    UPPER(LEFT(customer_name, 1)) + LOWER(SUBSTRING(customer_name, 2, LEN(customer_name))) AS customer_name,
+    CASE 
+        WHEN country IS NULL OR LTRIM(RTRIM(country)) = '' 
+            THEN 'UNKNOWN'
+        ELSE UPPER(country)
+    END AS country
+FROM bronze_customers
