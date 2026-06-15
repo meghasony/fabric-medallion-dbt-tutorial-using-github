c6261ce15f43e0833bf652cb124d9ed4fdb07c4e
Megha Sony <meghasony1997@gmail.com>
2026-06-15 17:02:03 +0530

    Update silver_customers.sql

--- models/silver/silver_customers.sql
+++ models/silver/silver_customers.sql
@@ -2,10 +2,10 @@
 
 SELECT
     customer_id,
-    UPPER(LEFT(customer_name, 1)) + LOWER(SUBSTRING(customer_name, 2, LEN(customer_name))) AS customer_name,
+    customer_name,
     CASE 
         WHEN country IS NULL OR LTRIM(RTRIM(country)) = '' 
             THEN 'UNKNOWN'
         ELSE UPPER(country)
     END AS country
-FROM bronze_customers
+FROM {{ source('bronze', 'customers') }}
